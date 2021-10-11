74bf59a843922e9ee414c07cadca5ac15ff8b84a
Ameen Alade <alade.ameen@venturegardengroup.com>
2021-10-11 19:38:19 +0100

    Linked list problems

---
+++ Linked List/linked-list-intersection.js
@@ -0,0 +1,8 @@
+var getIntersectionNode = function(headA, headB) {
+    let a = headA, b = headB
+    while (a !== b) {
+        a = a ? a.next : headB
+        b = b ? b.next : headA
+    }
+    return a
+};
